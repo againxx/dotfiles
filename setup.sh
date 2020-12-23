fcae6029c1ec65b8715f8f954a7f801b095fab5c
Xi Xia <againxx@mail.ustc.edu.cn>
2020-12-23 21:07:26 +0800

    Update setup.sh to git clone tmux-tpm

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
 DOTFILES_DIR=$(dirname $(realpath $0))
 
-ln -sf $DOTFILES_DIR/vimrc/.vimrc ~/.vimrc
-ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
-ln -sf $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
-ln -sf $DOTFILES_DIR/.gitconfig ~/.gitconfig
-ln -sf $DOTFILES_DIR/.gitignore_global ~/.gitignore_global
+ln -sf $DOTFILES_DIR/vimrc/.vimrc ~/
+ln -sf $DOTFILES_DIR/.zshrc ~/
+ln -sf $DOTFILES_DIR/.tmux.conf ~/
+ln -sf $DOTFILES_DIR/.gitconfig ~/
+ln -sf $DOTFILES_DIR/.gitignore_global ~/
 
 mkdir -p ~/.config/gtk-3.0
-ln -sf $DOTFILES_DIR/gtk.css ~/.config/gtk-3.0/gtk.css
-ln -sf $DOTFILES_DIR/alacritty ~/.config/alacritty
-ln -sf $DOTFILES_DIR/kitty ~/.config/kitty
-ln -sf $DOTFILES_DIR/ranger ~/.config/ranger
+ln -sf $DOTFILES_DIR/gtk.css ~/.config/gtk-3.0/
+ln -sf $DOTFILES_DIR/alacritty ~/.config/
+ln -sf $DOTFILES_DIR/kitty ~/.config/
+ln -sf $DOTFILES_DIR/ranger ~/.config/
+
+if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
