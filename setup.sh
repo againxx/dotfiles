614efe849fb4d7298ce46408b38b890d3599a471
Xi Xia <againxx@mail.ustc.edu.cn>
2020-12-26 13:49:15 +0800

    Only track lazygit config.yml, make a new dir in .config/jesseduffield/lazygit

--- setup.sh
+++ setup.sh
@@ -8,11 +8,12 @@ ln -sf $DOTFILES_DIR/.gitconfig ~/
 ln -sf $DOTFILES_DIR/.gitignore_global ~/
 
 mkdir -p ~/.config/gtk-3.0
+mkdir -p ~/.config/jesseduffield/lazygit
 ln -sf $DOTFILES_DIR/gtk.css ~/.config/gtk-3.0/
 ln -sf $DOTFILES_DIR/alacritty ~/.config/
 ln -sf $DOTFILES_DIR/kitty ~/.config/
 ln -sf $DOTFILES_DIR/ranger ~/.config/
-ln -sf $DOTFILES_DIR/jesseduffield ~/.config/
+ln -sf $DOTFILES_DIR/lazygit/config.yml ~/.config/jesseduffield/lazygit/
 
 if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
