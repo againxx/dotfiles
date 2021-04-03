63901c125b8dd0efd4b61615ff9a233439ac0e57
Xi Xia <againxx@mail.ustc.edu.cn>
2021-04-03 23:54:44 +0800

    Add some new rust command tools in homebrew

--- setup.sh
+++ setup.sh
@@ -56,6 +56,10 @@ if [[ -x "$(command -v brew)" ]]; then
     brew_pack[valgrind]=valgrind
     brew_pack[duf]=duf
     brew_pack[ncdu]=ncdu
+    brew_pack[exa]=exa
+    brew_pack[tldr]=tealdeer
+    brew_pack[bat]=bat
+    brew_pack[procs]=procs
 
     for exec_name in "${!brew_pack[@]}"; do
         if ! command -v $exec_name &> /dev/null; then
