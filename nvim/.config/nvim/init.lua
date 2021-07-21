da391aa1765d9ad0eba0ca59cc4bdcc1fae43a2d
Xi Xia <againxx@mail.ustc.edu.cn>
2021-07-21 18:49:07 +0800

    Install packages after bootstrapping

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,6 +7,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
   execute 'packadd packer.nvim'
   require('plugins')
+  require('packer').install()
 end
 
 vim.g.mapleader = ' '
