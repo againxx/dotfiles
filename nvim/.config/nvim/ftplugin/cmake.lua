5109dc02b21603ac2f3f51261bdfe0823d36c74b
Xi Xia <againxx@mail.ustc.edu.cn>
2021-08-24 00:36:48 +0800

    Add switch definitions for cmake

--- nvim/.config/nvim/ftplugin/cmake.lua
+++ nvim/.config/nvim/ftplugin/cmake.lua
@@ -4,3 +4,8 @@ vim.defer_fn(function()
   end)
   require('xx.catkin').detect_ws_root()
 end, 500)
+
+vim.b.switch_custom_definitions = {
+  vim.fn['switch#NormalizedCaseWords']({'on', 'off'}),
+  vim.fn['switch#NormalizedCaseWords']({'yes', 'no'}),
+}
