9459752046db6db5ed6d2776fe4d5afadd29cf2d
Xi Xia <againxx@mail.ustc.edu.cn>
2021-08-27 23:09:34 +0800

    Change python snippet stype to sphinx

--- nvim/.config/nvim/plugin/snippets.lua
+++ nvim/.config/nvim/plugin/snippets.lua
@@ -1,7 +1,7 @@
 vim.g.ulti_expand_res = 0
 vim.g.ultisnips_python_quoting_style = 'double'
 vim.g.ultisnips_python_triple_quoting_style = 'double'
-vim.g.ultisnips_python_style = 'stable-baselines'
+vim.g.ultisnips_python_style = 'sphinx'
 vim.g.ultisnips_cpp_style = 'google'
 
 -- Use <C-l> to expand ultisnips or save visual selected content
