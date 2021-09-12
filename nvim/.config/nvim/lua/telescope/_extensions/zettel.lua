9807d368b40730f0335e8109459518a658757998
Xi Xia <againxx@mail.ustc.edu.cn>
2021-09-12 21:58:45 +0800

    Update some code in zettel.lua

--- nvim/.config/nvim/lua/telescope/_extensions/zettel.lua
+++ nvim/.config/nvim/lua/telescope/_extensions/zettel.lua
@@ -13,12 +13,59 @@ local Path = require('plenary.path')
 
 local conf = require('telescope.config').values
 
+-- Parse file. Returns list of all anchors and their position
+local get_anchors = function(file_path, syntax)
+  local anchors = {}
+  if vim.fn.filereadable(file_path) == false then
+    return anchors
+  end
+
+  local header_regex = vim.fn['vimwiki#vars#get_syntaxlocal']('header_search', syntax)
+  local bold_regex = vim.fn['vimwiki#vars#get_syntaxlocal']('bold_search', syntax)
+  local tag_regex = vim.fn['vimwiki#vars#get_syntaxlocal']('tag_search', syntax)
+  local lnum = 1
+  for line in io.lines(file_path) do
+    local header_match = vim.fn.matchlist(line, header_regex)
+    if #header_match ~= 0 then
+      table.insert(anchors, {vim.fn['vimwiki#base#normalize_anchor'](header_match[3]), lnum})
+    end
+
+    -- there can be serveral bold text in one line
+    local bold_count = 1
+    while true do
+      local bold_text = vim.fn.matchstr(line, bold_regex, 0, bold_count)
+      if bold_text == '' then
+        break
+      end
+      table.insert(anchors, {bold_text, lnum})
+      bold_count = bold_count + 1
+    end
+
+    -- there can be serveral tags in one line
+    local tag_count = 1
+    while true do
+      local tag_group_text = vim.fn.matchstr(line, tag_regex, 0, tag_count)
+      if tag_group_text == '' then
+        break
+      end
+      local sep = vim.fn['vimwiki#vars#get_syntaxlocal']('tag_format', syntax).sep
+      for _, tag_text in ipairs(vim.split(tag_group_text, sep)) do
+        table.insert(anchors, {tag_text, lnum})
+      end
+      tag_count = tag_count + 1
+    end
+
+    lnum = lnum + 1
+  end
+  return anchors
+end
+
 local staged_finder = function(opts)
   opts.wiki_nr = opts.wiki_nr or vim.fn['vimwiki#vars#get_bufferlocal']('wiki_nr')
   opts.wiki_root = opts.wiki_root or vim.fn['vimwiki#vars#get_wikilocal']('path')
   opts.wiki_ext = opts.wiki_ext or vim.fn['vimwiki#vars#get_wikilocal']('ext')
 
-  local gen_from_wiki_name = function(opts)
+  local gen_from_wiki_name = function()
     local displayer = entry_display.create {
       separator = " ",
       items = {
@@ -41,18 +88,16 @@ local staged_finder = function(opts)
     return function(wiki_name)
       return {
         ordinal = wiki_name,
-        -- Path to package.xml for file preview
         -- path = Path:new(wiki_root .. wiki_name:sub(2) .. wiki_ext):absolute(),
         value = wiki_name:sub(2),
         -- Path to wiki file
         filename = Path:new(opts.wiki_root .. wiki_name:sub(2) .. opts.wiki_ext):absolute(),
-        -- pkg_type = type,
         display = make_display
       }
     end
   end
 
-  local gen_from_wiki_anchor = function(opts)
+  local gen_from_wiki_anchor = function()
     local displayer = entry_display.create {
       separator = "▏",
       items = {
@@ -70,12 +115,11 @@ local staged_finder = function(opts)
 
     return function(anchor)
       return {
-        ordinal = anchor,
-        -- Path to package.xml for file preview
-        -- path = Path:new(wiki_root .. wiki_name:sub(2) .. wiki_ext):absolute(),
-        value = anchor,
+        ordinal = anchor[1],
+        value = anchor[1],
         -- Path to wiki file
-        -- filename = Path:new(opts.wiki_root .. wiki_name:sub(2) .. opts.wiki_ext):absolute(),
+        filename = Path:new(opts.wiki_root .. opts.wiki_name:sub(2) .. opts.wiki_ext):absolute(),
+        lnum = anchor[2],
         -- pkg_type = type,
         display = make_display
       }
@@ -90,9 +134,7 @@ local staged_finder = function(opts)
     local link_infos = vim.fn['vimwiki#base#resolve_link'](opts.wiki_name .. '#', opts.wiki_full_path)
     local wiki_file = link_infos.filename
     local syntax = vim.fn['vimwiki#vars#get_wikilocal']('syntax', link_infos.index)
-    P(syntax)
-    local anchors = vim.fn['vimwiki#base#get_anchors'](wiki_file, syntax)
-    P(anchors)
+    local anchors = get_anchors(wiki_file, syntax)
     return finders.new_table { results = anchors, entry_maker = gen_from_wiki_anchor(opts) }
   end
 
@@ -101,7 +143,7 @@ end
 local zettel_insert_link = function(opts)
   opts = opts or {}
   pickers.new (opts, {
-    prompt_title = 'Open zettel notes',
+    prompt_title = 'Insert zettel notes',
     finder = staged_finder(opts),
     sorter = sorters.get_generic_fuzzy_sorter(),
     previewer = conf.file_previewer(opts),
@@ -112,13 +154,9 @@ local zettel_insert_link = function(opts)
         local _, col = vim.api.nvim_win_get_cursor(0)
         local text_before_cursor = vim.api.nvim_get_current_line():sub(1, col)
         if text_before_cursor:match('.*%[%[$') then
-          vim.schedule(function()
-            vim.api.nvim_put({ selection.value }, 'c', true, false)
-          end)
+          vim.api.nvim_put({ selection.value }, 'c', true, true)
         else
-          vim.schedule(function()
-            vim.api.nvim_put({ '[[' .. selection.value }, 'c', true, false)
-          end)
+          vim.api.nvim_put({ '[[' .. selection.value }, 'c', true, true)
         end
       end)
 
@@ -127,7 +165,7 @@ local zettel_insert_link = function(opts)
         opts.wiki_name = action_state.get_selected_entry().value
         opts.wiki_full_path = action_state.get_selected_entry().filename
         local current_picker = action_state.get_current_picker(prompt_bufnr)
-        current_picker.previewer = false
+        current_picker.previewer = conf.qflist_previewer(opts)
         current_picker:refresh(staged_finder(opts), { reset_prompt = true })
       end
 
