d15ad5e4b71d9f937b239fbffe482ca097e32bb5
Xi Xia <againxx@mail.ustc.edu.cn>
2021-10-09 11:34:13 +0800

    Update rust dap configuration to use lldb

--- nvim/.config/nvim/lua/xx/dap/rust.lua
+++ nvim/.config/nvim/lua/xx/dap/rust.lua
@@ -1,50 +1,48 @@
-local dap = require('dap')
+local dap = require "dap"
 
-dap.adapters.cppdbg = {
-  type = 'executable',
-  command = vim.env.HOME .. '/Manually_Installed/cpptools-linux/extension/debugAdapters/bin/OpenDebugAD7',
+dap.adapters.lldb = {
+  type = "executable",
+  command = "/usr/bin/lldb-vscode",
+  name = "lldb",
 }
 
 dap.configurations.rust = {
   {
-    name = "Launch (GDB)",
-    type = "cppdbg",
+    name = "Launch",
+    type = "lldb",
     request = "launch",
     program = function()
       local has_last_executable = false
-      if vim.b.dap_last_executable
+      if
+        vim.b.dap_last_executable
         and #vim.b.dap_last_executable > 0
-        and vim.fn.executable(vim.b.dap_last_executable) > 0 then
+        and vim.fn.executable(vim.b.dap_last_executable) > 0
+      then
         has_last_executable = true
       end
-      if not (vim.b.catkin_workspace and has_last_executable) then
-        local default = has_last_executable and vim.b.dap_last_executable or vim.fn.getcwd() .. '/'
-        vim.b.dap_last_executable = vim.fn.input('Path to executable: ', default, 'file')
-      end
-      assert(#vim.b.dap_last_executable > 0 and vim.fn.executable(vim.b.dap_last_executable) > 0, "Please provide valid executable file")
+      local default = has_last_executable and vim.b.dap_last_executable or vim.fn.getcwd() .. "/"
+      vim.b.dap_last_executable = vim.fn.input("Path to executable: ", default, "file")
+      assert(
+        #vim.b.dap_last_executable > 0 and vim.fn.executable(vim.b.dap_last_executable) > 0,
+        "Please provide valid executable file"
+      )
       return vim.b.dap_last_executable
     end,
-    MIMode = 'gdb',
-    cwd = '${workspaceFolder}',
-    stopOnEntry = true,
-    setupCommands = {
-      {
-        description = 'Enable pretty-printing for gdb',
-        text = '-enable-pretty-printing',
-        ignoreFailures = true
-      },
-    }
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+    args = {},
+    runInTerminal = true,
   },
 }
 
-require('dapui').setup({
+require("dapui").setup {
   mappings = {
     -- Use a table to apply multiple mappings
-    expand = { '<CR>', '<Tab>', '<2-LeftMouse>' },
-    open = 'o',
-    remove = 'd',
-    edit = 'e',
-    repl = 'r',
+    expand = { "<CR>", "<Tab>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
   },
   sidebar = {
     open_on_start = false,
@@ -52,24 +50,24 @@ require('dapui').setup({
     elements = {
       -- Provide as ID strings or tables with "id" and "size" keys
       {
-        id = 'stacks',
+        id = "stacks",
         size = 0.33, -- Can be float or integer > 1
       },
-      { id = 'breakpoints', size = 0.33 },
-      { id = 'watches', size = 0.33 },
+      { id = "breakpoints", size = 0.33 },
+      { id = "watches", size = 0.33 },
     },
     size = 40,
-    position = 'left', -- Can be "left" or "right"
+    position = "left", -- Can be "left" or "right"
   },
   tray = {
     open_on_start = false,
-    elements = { 'scopes' },
+    elements = { "scopes" },
     size = 20,
-    position = 'bottom', -- Can be "bottom" or "top"
+    position = "bottom", -- Can be "bottom" or "top"
   },
   floating = {
     max_height = 0.6, -- These can be integers or a float between 0 and 1.
     max_width = 0.6, -- Floats will be treated as percentage of your screen.
   },
   windows = { indent = 1 },
-})
+}
