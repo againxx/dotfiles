de0fed5534c3388108d69ed589b7eda188fcb51b
Xi Xia <againxx@mail.ustc.edu.cn>
2021-10-17 21:21:24 +0800

    Add set_debug_args for dap-python

--- nvim/.config/nvim/lua/xx/dap/python.lua
+++ nvim/.config/nvim/lua/xx/dap/python.lua
@@ -1,20 +1,18 @@
-local python_path = '~/.virtualenvs/debugpy/bin/python'
+local python_path = "~/.virtualenvs/debugpy/bin/python"
 local conda_prefix = vim.env.CONDA_PREFIX
 if conda_prefix then
-  python_path = conda_prefix .. '/bin/python'
+  python_path = conda_prefix .. "/bin/python"
 end
 
+require("dap-python").setup(python_path)
+require("dap-python").test_runner = "pytest"
 
-require('dap-python').setup(python_path)
-require('dap-python').test_runner = 'pytest'
-
-local dap = require('dap')
+local dap = require "dap"
 for i = 1, #dap.configurations.python do
-  dap.configurations.python[i] = vim.tbl_extend('force',
-    dap.configurations.python[i], { justMyCode = false })
+  dap.configurations.python[i] = vim.tbl_extend("force", dap.configurations.python[i], { justMyCode = false })
 end
 
-require("dapui").setup({
+require("dapui").setup {
   sidebar = {
     open_on_start = false,
     -- You can change the order of elements in the sidebar
@@ -32,7 +30,7 @@ require("dapui").setup({
   },
   tray = {
     open_on_start = false,
-    elements = { 'scopes' },
+    elements = { "scopes" },
     size = 15,
     position = "bottom", -- Can be "bottom" or "top"
   },
@@ -41,11 +39,26 @@ require("dapui").setup({
     max_width = 0.6, -- Floats will be treated as percentage of your screen.
   },
   windows = { indent = 1 },
-})
+}
 
-local wk = require('which-key')
+local set_debug_args = function()
+  local default = vim.g.dap_python_args and table.concat(vim.g.dap_python_args, " ") or ""
+  vim.g.dap_python_args = vim.split(vim.fn.input("Program arguments: ", default, "file"), " ")
+  for i = 1, #dap.configurations.python do
+    dap.configurations.python[i] = vim.tbl_extend(
+      "force",
+      dap.configurations.python[i],
+      { args = vim.g.dap_python_args }
+    )
+  end
+end
+
+local wk = require "which-key"
 
 wk.register({
-  n = { "<cmd>lua require('dap-python').test_method()<cr>", 'Test method' },
-  s = { "<cmd>lua require('dap-python').test_class()<cr>", 'Test class' },
-}, { prefix = '<leader>d' })
+  n = { "<cmd>lua require('dap-python').test_method()<cr>", "Test method" },
+  s = { "<cmd>lua require('dap-python').test_class()<cr>", "Test class" },
+  a = { set_debug_args, "Set debug arguments" },
+}, {
+  prefix = "<leader>d",
+})
