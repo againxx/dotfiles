4b80955373c5ff44ca32089df1a56473a3e4448d
Xi Xia <againxx@mail.ustc.edu.cn>
2022-02-10 19:59:51 +0800

    Add snippet for general includes

--- nvim/.config/nvim/lua/xx/snippets/cpp.lua
+++ nvim/.config/nvim/lua/xx/snippets/cpp.lua
@@ -1,8 +1,11 @@
 local ls = require "luasnip"
 local s = ls.snippet
+local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
+local c = ls.choice_node
+local r = ls.restore_node
 local fmta = require("luasnip.extras.fmt").fmta
 local conds = require("luasnip.extras.expand_conditions")
 local get_left_curly_brace_style = require("xx.snippets.utils").get_left_curly_brace_style
@@ -72,6 +75,14 @@ local snippets = {
     condition = conds.line_begin,
     show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incben") end,
   }),
+  s({ trig = "^%s*inc(%w+)", regTrig = true, hidden = true }, {
+    t("#include "),
+    c(1, {
+      sn(nil, { t("<"), r(1, "header_file", { f(function(_, parent) return parent.snippet.captures[1] end), i(1) }), t(">") }),
+      sn(nil, { t('"'), r(1, "header_file"), t('"') }),
+    })
+  }
+  ),
   s("cout", {
     t("std::cout << "),
     f(function(_, snip) return snip.env.SELECT_DEDENT end, {}),
