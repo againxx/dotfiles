b7b0a069f79f2afcb51784c7736ecad53dfb3e0a
Xi Xia <againxx@mail.ustc.edu.cn>
2022-02-13 11:23:01 +0800

    Refactor snippets for cpp

--- nvim/.config/nvim/lua/xx/snippets/cpp.lua
+++ nvim/.config/nvim/lua/xx/snippets/cpp.lua
@@ -14,67 +14,29 @@ ls.filetype_extend("cpp", {"c"})
 
 local show_line_begin = require("xx.snippets.utils").show_line_begin
 
+local include_std_header = function(trigger, header_file)
+  return s(trigger, t("#include <" .. header_file .. ">"), {
+    condition = conds.line_begin,
+    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, trigger) end,
+  })
+end
+
 local snippets = {
-  s("incio", t("#include <iostream>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incio") end,
-  }),
-  s("incvec", t("#include <vector>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incvec") end,
-  }),
-  s("incarr", t("#include <array>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incarr") end,
-  }),
-  s("incls", t("#include <list>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incls") end,
-  }),
-  s("incal", t("#include <algorithm>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incal") end,
-  }),
-  s("incstr", t("#include <string>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incstr") end,
-  }),
-  s("incmap", t("#include <map>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incmap") end,
-  }),
-  s("incset", t("#include <set>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incset") end,
-  }),
-  s("incumap", t("#include <unordered_map>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incumap") end,
-  }),
-  s("incexc", t("#include <stdexcept>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incexc") end,
-  }),
-  s("incmem", t("#include <memory>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incmem") end,
-  }),
-  s("incros", t("#include <ros/ros.h>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incros") end,
-  }),
-  s("inctest", t("#include <gtest/gtest.h>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "inctest") end,
-  }),
-  s("incmock", t("#include <gmock/gmock.h>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incmock") end,
-  }),
-  s("incben", t("#include <benchmark/benchmark.h>"), {
-    condition = conds.line_begin,
-    show_condition = function(line_to_cursor) return show_line_begin(line_to_cursor, "incben") end,
-  }),
+  include_std_header("incio", "iostream"),
+  include_std_header("incvec", "vector"),
+  include_std_header("incarr", "array"),
+  include_std_header("incls", "list"),
+  include_std_header("incal", "algorithm"),
+  include_std_header("incstr", "string"),
+  include_std_header("incmap", "map"),
+  include_std_header("incset", "set"),
+  include_std_header("incumap", "unordered_map"),
+  include_std_header("incexc", "stdexcept"),
+  include_std_header("incmem", "memory"),
+  include_std_header("incros", "ros/ros.h"),
+  include_std_header("inctest", "gtest/gtest.h"),
+  include_std_header("incmock", "gmock/gmock.h"),
+  include_std_header("incben", "benchmark/benchmark.h"),
   s({ trig = "^%s*inc(%w+)", regTrig = true, hidden = true }, {
     t("#include "),
     c(1, {
