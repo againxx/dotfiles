efd399a91cb7ab555493f174049be7a81f69c7e9
Xi Xia <againxx@mail.ustc.edu.cn>
2022-02-14 10:23:35 +0800

    Make <c-n> and <c-p> more intelligent in command line mode

--- nvim/.config/nvim/plugin/completion.lua
+++ nvim/.config/nvim/plugin/completion.lua
@@ -1,93 +1,93 @@
-local lspkind = require('lspkind')
-lspkind.init({
+local lspkind = require "lspkind"
+lspkind.init {
   symbol_map = {
-        Function = "",
-        Constructor = "",
-        Variable = "",
-        Snippet = "",
-        Reference = "渚",
-      },
-})
+    Function = "",
+    Constructor = "",
+    Variable = "",
+    Snippet = "",
+    Reference = "渚",
+  },
+}
 
-local cmp = require('cmp')
-local luasnip = require("luasnip")
+local cmp = require "cmp"
+local luasnip = require "luasnip"
 
 cmp.setup {
   snippet = {
     expand = function(args)
       -- vim.fn['UltiSnips#Anon'](args.body)
-      require('luasnip').lsp_expand(args.body)
+      require("luasnip").lsp_expand(args.body)
     end,
   },
   mapping = {
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ["<Tab>"] = cmp.mapping(function(fallback)
       -- vim.fn['UltiSnips#ExpandSnippet']()
       if luasnip.expand() then
         cmp.close()
       elseif cmp.visible() then
-        cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
+        cmp.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true }
       else
         fallback()
       end
-    end, {'i', 's'}),
-    ['<C-j>'] = cmp.mapping(function(fallback)
+    end, { "i", "s" }),
+    ["<C-j>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       else
         fallback()
       end
-    end, {'i', 'c'}),
-    ['<C-k>'] = cmp.mapping(function(fallback)
+    end, { "i", "c" }),
+    ["<C-k>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       else
         fallback()
       end
-    end, {'i', 'c'}),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    end, { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
     -- Use <c-space> to trigger completion.
-    ['<C-space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-e>'] = cmp.mapping({
+    ["<C-space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-e>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
-    }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    },
+    ["<CR>"] = cmp.mapping.confirm { select = true },
   },
   sources = {
-    { name = 'nvim_lsp' },
+    { name = "nvim_lsp" },
     -- { name = 'ultisnips' },
-    { name = 'luasnip' },
-    { name = 'nvim_lua' },
-    { name = 'path' },
+    { name = "luasnip" },
+    { name = "nvim_lua" },
+    { name = "path" },
     {
-      name = 'buffer',
+      name = "buffer",
       option = {
         get_bufnrs = function()
           return vim.api.nvim_list_bufs()
-        end
-      }
+        end,
+      },
     },
-    { name = 'tmux' }
+    { name = "tmux" },
   },
   formatting = {
-      format = lspkind.cmp_format({
-        with_text = true,
-        maxwidth = 50,
-        menu = {
-          buffer = "[buf]",
-          nvim_lsp = "[lsp]",
-          nvim_lua = "[api]",
-          path = "[path]",
-          -- ultisnips = "[snip]",
-          luasnip = "[snip]",
-          tmux = "[tmux]",
-        },
-      })
+    format = lspkind.cmp_format {
+      with_text = true,
+      maxwidth = 50,
+      menu = {
+        buffer = "[buf]",
+        nvim_lsp = "[lsp]",
+        nvim_lua = "[api]",
+        path = "[path]",
+        -- ultisnips = "[snip]",
+        luasnip = "[snip]",
+        tmux = "[tmux]",
+      },
+    },
   },
   documentation = {
     -- border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-    winhighlight = 'NormalFloat:CmpDocNormal',
+    winhighlight = "NormalFloat:CmpDocNormal",
   },
   experimental = {
     native_menu = false,
@@ -96,19 +96,39 @@ cmp.setup {
 }
 
 -- Use buffer source for `/`
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline("/", {
+  mapping = {
+    ["<c-n>"] = cmp.mapping(function(fallback)
+      cmp.close()
+      fallback()
+    end, {"i", "c"}),
+    ["<c-p>"] = cmp.mapping(function(fallback)
+      cmp.close()
+      fallback()
+    end, {"i", "c"}),
+  },
   sources = {
-    { name = 'buffer' }
+    { name = "buffer" },
   },
   view = {
-    entries = { name = 'wildmenu' }
-  }
+    entries = { name = "wildmenu" },
+  },
 })
 
-cmp.setup.cmdline(':', {
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline', keyword_length = 3 }
-    })
+cmp.setup.cmdline(":", {
+  mapping = {
+    ["<c-n>"] = cmp.mapping(function(fallback)
+      cmp.close()
+      fallback()
+    end, {"i", "c"}),
+    ["<c-p>"] = cmp.mapping(function(fallback)
+      cmp.close()
+      fallback()
+    end, {"i", "c"}),
+  },
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline", keyword_length = 3 },
+  }),
 })
