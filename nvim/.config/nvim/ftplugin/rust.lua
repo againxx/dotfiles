0ba341f17afc871b11d55928dc8ebd890ee67302
Xi Xia <againxx@mail.ustc.edu.cn>
2022-02-20 09:31:51 +0800

    Add autopair for $ in tex

--- nvim/.config/nvim/ftplugin/rust.lua
+++ nvim/.config/nvim/ftplugin/rust.lua
@@ -1,7 +1,6 @@
 -- add < into autopairs
 local Rule = require('nvim-autopairs.rule')
 local npairs = require('nvim-autopairs')
-local cond = require('nvim-autopairs.conds')
 
 npairs.add_rule(Rule("<", ">", "rust"):with_move(function(opts)
   if opts.char == ">" then
--- nvim/.config/nvim/ftplugin/tex.lua
+++ nvim/.config/nvim/ftplugin/tex.lua
@@ -62,7 +62,13 @@ wk.register({
   buffer = vim.api.nvim_get_current_buf(),
 })
 
+-- add $ into autopairs
 local Rule = require('nvim-autopairs.rule')
 local npairs = require('nvim-autopairs')
 
-npairs.add_rule(Rule("$","$","tex"))
+npairs.add_rule(Rule("$","$","tex"):with_move(function(opts)
+  if opts.char == "$" then
+    return true
+  end
+  return false
+end))
