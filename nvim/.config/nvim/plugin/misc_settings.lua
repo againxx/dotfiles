8a28619ef82e46c4c57b544e089de76b5cc4a77c
Xi Xia <againxx@mail.ustc.edu.cn>
2022-02-22 23:37:55 +0800

    Change indent char

--- nvim/.config/nvim/plugin/misc_settings.lua
+++ nvim/.config/nvim/plugin/misc_settings.lua
@@ -12,7 +12,7 @@ vim.g.rainbow_conf = {
 }
 
 -- indent_blankline
-vim.g.indent_blankline_char = '┃' -- │
+vim.g.indent_blankline_char = '│' -- │┃
 vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_show_first_indent_level = false
 vim.g.indent_blankline_show_trailing_blankline_indent = false
