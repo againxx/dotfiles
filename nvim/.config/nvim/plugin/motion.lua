5238c102c82d668d42e344ea15392ec3a0e720ec
Xi Xia <againxx@mail.ustc.edu.cn>
2022-04-17 22:32:36 +0800

    Manually add matchup key mappings

--- nvim/.config/nvim/plugin/motion.lua
+++ nvim/.config/nvim/plugin/motion.lua
@@ -1,30 +1,21 @@
-require('neoscroll').setup({
-  mappings = { '<C-u>', '<C-d>', 'zt', 'zz', 'zb' },
-  easing_function = 'sine'
-})
+require("neoscroll").setup {
+  mappings = { "<C-u>", "<C-d>", "zt", "zz", "zb" },
+  easing_function = "sine",
+}
 
 local keymap = vim.keymap
 
+local opts = { silent = true, nowait = true }
+local plug_opts = { silent = true, nowait = true, remap = true }
+
 -- Scroll floating window up and down
-keymap.set('n', '<C-f>',
-  "<cmd>lua require('xx.neoscroll')(vim.api.nvim_win_get_height(0), true, 500)<cr>",
-  { silent = true, nowait = true, noremap = true })
-keymap.set('n', '<C-b>',
-  "<cmd>lua require('xx.neoscroll')(-vim.api.nvim_win_get_height(0), true, 500)<cr>",
-  { silent = true, nowait = true, noremap = true })
-keymap.set('v', '<C-f>',
-  "<cmd>lua require('xx.neoscroll')(vim.api.nvim_win_get_height(0), true, 500)<cr>",
-  { silent = true, nowait = true, noremap = true })
-keymap.set('v', '<C-b>',
-  "<cmd>lua require('xx.neoscroll')(-vim.api.nvim_win_get_height(0), true, 500)<cr>",
-  { silent = true, nowait = true, noremap = true })
-keymap.set('n', '<C-y>', "<cmd>lua require('neoscroll').scroll(-3, false, 80, nil)<cr>",
-  { silent = true, nowait = true, noremap = true })
-keymap.set('n', '<C-e>', "<cmd>lua require('neoscroll').scroll(3, false, 80, nil)<cr>",
-  { silent = true, nowait = true, noremap = true })
+keymap.set("n", "<C-f>", "<cmd>lua require('xx.neoscroll')(vim.api.nvim_win_get_height(0), true, 500)<cr>", opts)
+keymap.set("n", "<C-b>", "<cmd>lua require('xx.neoscroll')(-vim.api.nvim_win_get_height(0), true, 500)<cr>", opts)
+keymap.set("v", "<C-f>", "<cmd>lua require('xx.neoscroll')(vim.api.nvim_win_get_height(0), true, 500)<cr>", opts)
+keymap.set("v", "<C-b>", "<cmd>lua require('xx.neoscroll')(-vim.api.nvim_win_get_height(0), true, 500)<cr>", opts)
+keymap.set("n", "<C-y>", "<cmd>lua require('neoscroll').scroll(-3, false, 80, nil)<cr>", opts)
+keymap.set("n", "<C-e>", "<cmd>lua require('neoscroll').scroll(3, false, 80, nil)<cr>", opts)
 
 -- lightspeed 'cold' repeat
-keymap.set('n', ';', "<Plug>Lightspeed_;_ft",
-  { silent = true, nowait = true, noremap = false })
-keymap.set('n', ',', "<Plug>Lightspeed_,_ft",
-  { silent = true, nowait = true, noremap = false })
+keymap.set("n", ";", "<Plug>Lightspeed_;_ft", plug_opts)
+keymap.set("n", ",", "<Plug>Lightspeed_,_ft", plug_opts)
