6a7a8136362fabfd52cd3d69a001121d8095636c
Xi Xia <againxx@mail.ustc.edu.cn>
2022-06-04 00:29:24 +0800

    Use packer_plugins to construct binary path

--- nvim/.config/nvim/locals/jieba-ci/lua/jieba-ci.lua
+++ nvim/.config/nvim/locals/jieba-ci/lua/jieba-ci.lua
@@ -1,6 +1,6 @@
 local jobid = 0
 
-local bin_path = vim.env.HOME .. "/.config/nvim/locals/jieba.nvim/target/release/jieba_ci"
+local bin_path = packer_plugins["jieba-ci"].url .. "/target/release/jieba_ci"
 
 local init_rpc = function()
   if jobid == 0 then
