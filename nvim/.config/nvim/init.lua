44e15234d488ea8a22763bd07fbbdb10804e7af7
Xi Xia <againxx@mail.ustc.edu.cn>
2022-12-20 00:44:18 +0800

    Remove do_filetype_lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -21,7 +21,6 @@ vim.g.node_host_prog = "~/.config/yarn/global/node_modules/neovim/bin/cli.js"
 vim.g.loaded_python_provider = 0 -- disable python 2 support
 vim.g.loaded_perl_provider = 0 -- disable perl support
 vim.g.loaded_ruby_provider = 0 -- disable ruby support
-vim.g.do_filetype_lua = 1
 
 require "xx.treesitter"
 require "xx.globals"
