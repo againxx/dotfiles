ff60fc736f8f7f8efdeccbddf9d4d19cd2fe3193
Xi Xia <xiaxi@udeer.ai>
2023-12-18 10:33:11 +0800

    Fix spectre related mappings

--- nvim/.config/nvim/plugin/extra_leadermaps.lua
+++ nvim/.config/nvim/plugin/extra_leadermaps.lua
@@ -115,8 +115,8 @@ wk.register({
     t = { '<cmd>TSHighlightCapturesUnderCursor<cr>', 'TreeSitter highlighting under cursor' },
     T = { '<cmd>terminal tokei<cr>', 'Tokei code statistic' },
     f = { 'ga', 'Show different formats of character' },
-    w = { "<cmd>lua require('spectre').open_visual({select_word = true})", 'Search and replace current word' },
-    p = { "<cmd>lua require('spectre').open_file_search()", 'Search and replace in current file' },
+    w = { "<cmd>lua require('spectre').open_visual({select_word = true})<cr>", 'Search and replace current word' },
+    p = { "<cmd>lua require('spectre').open_file_search()<cr>", 'Search and replace in current file' },
     l = { "<cmd>lua require('persistence').load({ last = true })<cr>", 'Restore last session' },
     c = { "<cmd>lua require('persistence').load()<cr>", 'Restore session for current directory' },
   },
@@ -141,8 +141,9 @@ wk.register({
 
 wk.register({
   s = {
-    name = '+show',
-    f = { echo_formats_and_char, 'Show different formats of character' }
+    name = '+show/search',
+    f = { echo_formats_and_char, 'Show different formats of character' },
+    w = { "<cmd>lua require('spectre').open_visual()<cr>", 'Search and replace selected content' },
   },
   T = { '<Plug>TranslateWV', 'Translate' },
   S = { "<cmd>lua require('spectre').open_visual()<cr>", 'Search and replace' },
