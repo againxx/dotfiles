3300ebc7b247611e08f468300ae2750dfab0c61a
Xi Xia <againxx@mail.ustc.edu.cn>
2023-12-24 23:20:21 +0800

    Update config for ibl

--- nvim/.config/nvim/lua/xx/plugins.lua
+++ nvim/.config/nvim/lua/xx/plugins.lua
@@ -509,8 +509,10 @@ return {
       require("ibl").setup {
         exclude = {
           filetypes = { "dashboard" },
-        }
+        },
       }
+      local hooks = require "ibl.hooks"
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
     end,
   },
   { "rlue/vim-barbaric", ft = { "markdown", "vimwiki", "tex" } },
